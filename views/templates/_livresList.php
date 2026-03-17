dfe9e43949061dbfcbaef429251b309c4e33ddeb
Sacha Fauconnier <sacha.faucon@gmail.com>
2026-03-17 17:45:10 +0100

    page One book

--- views/templates/_livresList.php
+++ views/templates/_livresList.php
@@ -4,7 +4,11 @@
         <a href="index.php?action=detaillivre&id=<?= $livre->getId() ?>">
 
             <div class="livre-image">
-                <img src="<?= $livre->getImage() ?>">
+                        <img 
+                            src="<?= htmlspecialchars($livre->getImage() ?: 'https://cdn.paris.fr/paris/2021/10/04/huge-9394fb10a8ef69a7572e9e273521dfb8.jpeg') ?>" 
+                            onerror="this.onerror=null;this.src='https://cdn.paris.fr/paris/2021/10/04/huge-9394fb10a8ef69a7572e9e273521dfb8.jpeg';"
+                            alt="<?= htmlspecialchars($livre->getTitre()) ?>"
+                        >
             </div>
 
             <div class="livre-body">
--- views/templates/detailLivre.php
+++ views/templates/detailLivre.php
@@ -1,41 +1,58 @@
+
 <?php
-/** @var Livre $livre */
+/** @var array $livre */
 ?>
 
 <section class="livre-detail">
-
     <div class="livre-detail__container">
 
         <div class="livre-detail__image">
-            <img src="<?= $livre['image'] ?>" alt="<?= htmlspecialchars($livre['titre']) ?>">
-            
+            <img 
+                src="<?= htmlspecialchars($livre['image'] ?: 'https://cdn.paris.fr/paris/2021/10/04/huge-9394fb10a8ef69a7572e9e273521dfb8.jpeg') ?>"
+                onerror="this.onerror=null;this.src='https://cdn.paris.fr/paris/2021/10/04/huge-9394fb10a8ef69a7572e9e273521dfb8.jpeg';"
+                alt="<?= htmlspecialchars($livre['titre']) ?>"
+            >
         </div>
 
         <div class="livre-detail__content">
 
-            <h1 class="livre-detail__title">
+            <h2 class="livre-detail__title">
                 <?= htmlspecialchars($livre['titre']) ?>
-            </h1>
+            </h2>
 
             <p class="livre-detail__author">
                 par <?= htmlspecialchars($livre['auteur']) ?>
             </p>
 
-            <div class="livre-detail__description">
-                <?= nl2br(htmlspecialchars($livre['description'])) ?>
+            <div class="livre-detail__separator"></div>
+
+            <div class="livre-detail__block">
+                <p class="livre-detail__label">DESCRIPTION</p>
+                <div class="livre-detail__description">
+                    <?= nl2br(htmlspecialchars($livre['description'])) ?>
+                </div>
             </div>
 
-            <div class="livre-detail__proprietaire">
-                <strong>PROPRIÉTAIRE</strong><br><br>
-                <?= htmlspecialchars($livre['pseudo']) ?>
+            <div class="livre-detail__block">
+                <p class="livre-detail__label">PROPRIÉTAIRE</p>
+
+                <a href="" class="livre-detail__owner">
+                    <img 
+                        src="https://i.ibb.co/fVbxwgSY/Mask-group.png" 
+                        alt="Photo du propriétaire"
+                        class="livre-detail__owner-avatar"
+                    >
+                    <span class="livre-detail__owner-name">
+                        <?= htmlspecialchars($livre['pseudo']) ?>
+                    </span>
+                </a>
             </div>
 
-             <a href="" class="back-button">
+            <a href="index.php?action=messagerie&id=<?= (int)$livre['utilisateur_id'] ?>" class="livre-detail__button">
                 Envoyer un message
             </a>
 
         </div>
 
     </div>
-
 </section>
